8ea9ecd1ea76cffd46e39edf3206c6176b4248f1
uqba <muhammmad.uqba@codilitysolutions.com>
2022-03-04 12:36:28 +0500

    concatinate Dr before name() Boforesave

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,10 @@ class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments, dependent: :destroy
   accepts_nested_attributes_for :appointments
+
+  before_save :concatenate_name
+
+  def concatenate_name
+     self.name = "Dr.#{name.capitalize}"
+  end
 end
